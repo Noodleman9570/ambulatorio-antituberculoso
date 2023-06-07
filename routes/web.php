fd8460058f1c14a06b7af286eff16bf062aad25f
Noodleman9570 <kevinsaavedra55@gmail.com>
2023-06-07 14:22:29 -0400

    Se agregaron campos en el formulario de registro

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//Route::get('/');
+
 require __DIR__.'/auth.php';
