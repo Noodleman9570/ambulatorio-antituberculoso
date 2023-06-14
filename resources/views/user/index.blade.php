16e23f0b4acca56b3514924d40531719edaf0551
Noodleman9570 <kevinsaavedra55@gmail.com>
2023-06-14 00:34:47 -0400

    Se modifico el nombre del boto de nuevo usuarios

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -13,7 +13,7 @@
             <div class="block space-y-4 md:flex md:space-y-0 md:space-x-4">
                 <!-- Modal toggle -->
                 <button data-modal-target="createModal" data-modal-toggle="createModal" class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Small modal
+                Nuevo usuario
                 </button>
     
             </div>
